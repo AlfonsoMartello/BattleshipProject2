fe53d8191cdce92184e8519b4a9b6667bf85e77a
Karenw1004 <karenw0103@gmail.com>
2019-10-14 16:42:42 -0500

    UI for the score board and toggle button for scoreboard

--- Battleship/Assets/Scripts/Quit.cs
+++ Battleship/Assets/Scripts/Quit.cs
@@ -8,4 +8,16 @@ public class Quit : MonoBehaviour
         Debug.Log("EXIT THE GAME! BYE!!");
         Application.Quit();
     }
+
+    public GameObject ScoreBoardPanel;
+    public GameObject PlayerTurn;
+    public void openScoreBoardPanel()
+    {
+        if (ScoreBoardPanel != null)
+        {
+            bool isActive = ScoreBoardPanel.activeSelf;
+ 
+            ScoreBoardPanel.SetActive(!isActive);
+        }
+    }
 }
