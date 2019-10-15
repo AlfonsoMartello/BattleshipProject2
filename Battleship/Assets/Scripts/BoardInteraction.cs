5abad71d0f249dbf2096e8e6693bbe8e0710b981
Antonette <amgichohu@gmail.com>
2019-10-15 18:39:18 -0500

    ai hard beginning stuff

--- Battleship/Assets/Scripts/BoardInteraction.cs
+++ Battleship/Assets/Scripts/BoardInteraction.cs
@@ -144,10 +144,13 @@ public class BoardInteraction : MonoBehaviour
         }
         else if (Team2.aiDifficulty == 3)
         {
+            Debug.Log("You have chosen the hard way");
             AIhardGame();
         }
     }
+    
 
+      
     /**
     * @pre: A player presses the 'Confirm' button on the ship selection panel.
     * @post: Sets both boards to not be interactive, making sure players don't press buttons
@@ -332,6 +335,98 @@ public class BoardInteraction : MonoBehaviour
     }
     public void AIhardGame()
     {
+        bool hasPlayed1 = false, hasPlayed2 = false;
 
+        if (player1Turn) //actually player 2 turn
+        {
+            for (int i = 0; i < spacesAvailableBoard1.Length; i++)
+            {
+                Debug.Log("ai hard game");
+                //if (spacesAvailableBoard1[i].GetComponent<buttonController>().target == null) //if the selected button contains a ship part, fire 
+                //{
+                //    spacesAvailableBoard1[i].image.sprite = onClickIcons[0];
+                //    hasPlayed1 = true;
+                //}
+
+                //else
+
+                if (spacesAvailableBoard1[i].GetComponent<buttonController>().target != null)
+                {
+                    spacesAvailableBoard1[i].GetComponent<buttonController>().target.Hit(); //sets the ShipPartController as hit and checks if the player has lost
+                    spacesAvailableBoard1[i].image.sprite = onClickIcons[1];
+                    hasPlayed1 = true;
+                    break;
+                }
+
+            }
+        }
+        else
+        {
+            for (int i = 0; i < spacesAvailableBoard2.Length; i++)
+            {
+
+                if (spacesAvailableBoard2[i].image.sprite == onClickIcons[2])
+                {
+                    if (spacesAvailableBoard2[i].GetComponent<buttonController>().target == null)
+                    {
+                        spacesAvailableBoard2[i].image.sprite = onClickIcons[0];
+                        hasPlayed2 = true;
+                    }
+
+                    else
+                    {
+                        spacesAvailableBoard2[i].GetComponent<buttonController>().target.Hit();
+                        spacesAvailableBoard2[i].image.sprite = onClickIcons[1];
+                        hasPlayed2 = true;
+                    }
+                    break;
+                }
+            }
+
+            for (int i = 0; i < spacesAvailableBoard2.Length; i++)
+            {
+                if (hasPlayed2)
+                {
+                    spacesAvailableBoard2[i].interactable = false;
+                    if (spacesAvailableBoard1[i].image.sprite == null)
+                    {
+                        spacesAvailableBoard1[i].interactable = true;
+                    }
+                }
+                else
+                {
+                    if (spacesAvailableBoard2[i].image.sprite == null)
+                    {
+                        spacesAvailableBoard2[i].interactable = true;
+                    }
+                }
+            }
+
+            if (hasPlayed2)
+            {
+                player1Turn = true;
+                player1Board.GetComponent<Image>().enabled = true;
+                player2Turn = false;
+                player2Board.GetComponent<Image>().enabled = false;
+                battleshipGrids.SetActive(false);
+                gameUIPanel.SetActive(false);
+                switchPanel.SetActive(true);
+                playerTurn.GetComponent<Text>().text = "It's Player 2's Turn";
+                player1Shots++;
+            }
+            else
+            {
+                player1Turn = false;
+                player1Board.GetComponent<Image>().enabled = false;
+                player2Turn = true;
+                player2Board.GetComponent<Image>().enabled = true;
+                battleshipGrids.SetActive(false);
+                gameUIPanel.SetActive(false);
+                switchPanel.SetActive(true);
+                playerTurn.GetComponent<Text>().text = "It's Player 1's Turn";
+            }
+        }
     }
+            
+    
 }
